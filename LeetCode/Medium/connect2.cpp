a7278cdceb7ac98145363d65a6e2d8062fb3e0bd
Abhishek Udupa <56753465+abhishekaudupa@users.noreply.github.com>
2023-09-27 19:53:42 +0530

    Solve populating next right pointer 2 Solved the 'Populating Next Right Pointers in Each Node II' problem on Leetcode. https://leetcode.com/problems/populating-next-right-pointers-in-each-node-ii/

---
+++ LeetCode/Medium/connect2.cpp
@@ -0,0 +1,146 @@
+#include <iostream>
+
+/*
+   Populating Next Right Pointers in Each Node
+
+   You are given a perfect binary tree where all leaves are on the same level, and every parent has two children. The binary tree has the following definition:
+
+   struct Node {
+   int val;
+   Node *left;
+   Node *right;
+   Node *next;
+   }
+   Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
+
+   Initially, all next pointers are set to NULL.
+
+
+
+   Example 1:
+Input: root = [1,2,3,4,5,6,7]
+Output: [1,#,2,3,#,4,5,6,7,#]
+Explanation: Given the above perfect binary tree (Figure A), your function should populate each next pointer to point to its next right node, just like 
+in Figure B. The serialized output is in level order as connected by the next pointers, with '#' signifying the end of each level.
+
+Example 2:
+Input: root = []
+Output: []
+
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 212 - 1].
+-1000 <= Node.val <= 1000
+
+
+Follow-up:
+
+You may only use constant extra space.
+The recursive approach is fine. You may assume implicit stack space does not count as extra space for this problem.
+
+Solution: Recursive approach.
+		We simply connect the rightmost node of the left subtree to the leftmost node of the right subtree
+		level by level, starting at level below the root node at each recursive call.
+
+		At each recursive call,
+		1. We get the rightmost child of left child and the leftmost child of the right child of the root
+			at level 0, which are, the left and right children themselves.
+		2. In a loop that runs as long as we can find a left child, we do
+			a. Connect left child's next to the right child, if it's not already connected to a node.
+			b. Find the rightmost child of the left child one level below then previous.
+			c. Find the leftmost child of hte right child one level below than previous.
+		3. Recursive call for the root's left child.
+		4. Recursive call for the root's right child.
+		5. Return the root node.
+
+	There are some optimizations that I can think of but haven't bothered to implement.
+
+ */
+
+class Node {
+    public:
+	int val;
+	Node* left;
+	Node* right;
+	Node* next;
+
+	Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+	Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+	Node(int _val, Node* _left, Node* _right, Node* _next)
+	    : val(_val), left(_left), right(_right), next(_next) {}
+};
+
+class Solution {
+    public:
+	Node* getRightMostChild(Node* root, int level)
+	{
+	    if(!root)
+		return root;
+
+	    if(level > 1)
+	    {
+		--level;
+		auto rmc{ getRightMostChild(root->right, level) };
+		return rmc? rmc: getRightMostChild(root->left, level);
+	    }
+	    else
+	    {
+		if(level == 1)
+		    return (root->right)? root->right: root->left;
+		else
+		    return root;
+	    }
+	}
+
+	Node* getLeftMostChild(Node* root, int level)
+	{
+	    if(!root)
+		return root;
+
+	    if(level > 1)
+	    {
+		--level;
+		auto lmc{ getLeftMostChild(root->left, level) };
+		return lmc? lmc: getLeftMostChild(root->right, level);
+	    }
+	    else
+	    {
+		if(level == 1)
+		    return (root->left)? root->left: root->right;
+		else
+		    return root;
+	    }
+	}
+
+	Node* connect(Node* root) 
+	{
+	    if(!root)
+		return nullptr;
+
+	    int level{ };
+	    auto lc{ getRightMostChild(root->left, level) };
+	    auto rc{ getLeftMostChild(root->right, level) };
+
+	    while(lc)
+	    {
+		if(!(lc->next))
+		    lc->next = rc;
+		++level;
+		lc = getRightMostChild(root->left, level);
+		rc = getLeftMostChild(root->right, level);
+	    }
+
+	    connect(root->left);
+	    connect(root->right);
+
+	    return root;
+	}
+};
+
+int main()
+{
+}
+
